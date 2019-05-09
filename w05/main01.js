824843f98544bfdd98e4002abb87b15585399cd4
Satoki Tsuji <49426762+SatokiTsuji@users.noreply.github.com>
2019-05-09 18:50:14 +0900

    Add files via upload

---
+++ w05/main01.js
@@ -0,0 +1,104 @@
+function main()
+{
+    var width = 500;
+    var height = 500;
+
+    var scene = new THREE.Scene();
+
+    var fov = 45;
+    var aspect = width / height;
+    var near = 1;
+    var far = 1000;
+    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 0, 10 );
+    scene.add( camera );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+
+    var v = [];
+    var f = [];
+
+    var vertices = [
+        [1, 1, -1], // v0
+        [1, -1, -1], // v1
+        [1, -1, 1], // v2
+        [1, 1, 1],
+        [-1, -1, -1],
+        [-1, -1, 1],
+        [-1, 1, 1],
+        [-1, 1, -1]
+    ];
+    
+    var faces = [
+        [3, 1, 0], // f0: v0-v1-v2
+        [3, 2, 1],
+	    [0, 6, 3],
+	    [0, 7, 6],
+	    [4, 0, 1],
+	    [4, 7, 0],
+	    [6, 2, 3],
+	    [6, 5, 2],
+	    [2, 4, 1],
+	    [2, 5, 4],
+	    [6, 4, 5],
+	    [6, 7, 4]
+    ];
+
+    for( var i = 0; i <= 7; i++ ){	
+	v[i] = new THREE.Vector3().fromArray( vertices[i] );
+    }
+    for( var i = 0; i <= 11; i++ ){	
+	var id = faces[i];
+	f[i] = new THREE.Face3( id[0], id[1], id[2] );
+    }
+    
+    var geometry = new THREE.Geometry();
+    
+    for( var i = 0; i <= 7; i++ ){
+	geometry.vertices.push( v[i] );
+    }
+    for( var i = 0; i <= 11; i++ ){
+	geometry.faces.push( f[i] );
+    }
+    
+    var material = new THREE.MeshBasicMaterial();
+    material.vertexColors = THREE.FaceColors;
+    for( var i = 0; i <= 11; i++ ){
+	geometry.faces[i].color = new THREE.Color( 1, 0, 0 );
+    }
+
+
+    
+    
+    
+
+    
+    /*
+    geometry.computeFaceNormals();
+    material.side = THREE.DoubleSide;
+    */
+
+    /*
+    var material = new THREE.MeshBasicMaterial();
+    material.vertexColors = THREE.VertexColors;
+    geometry.faces[0].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[0].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[0].vertexColors.push(new THREE.Color(0,0,1));
+    */
+
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    loop();
+
+    function loop()
+    {
+        requestAnimationFrame( loop );
+        cube.rotation.x += 0.008;
+        cube.rotation.y += 0.008;
+
+        renderer.render( scene, camera );
+    }
+}
